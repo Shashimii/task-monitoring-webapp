c152bacfb7c8dd993815acf9ae263e77b186a73c
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-18 15:53:29 +0800

    # added add task modal button

--- resources/js/Pages/Task.jsx
+++ resources/js/Pages/Task.jsx
@@ -1,22 +1,31 @@
+import ModalToggle from "@/Components/Button/ModalToggle";
 import MainContainer from "@/Components/DivContainer/MainContainer";
 import PrimaryCard from "@/Components/DivContainer/PrimaryCard";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
 export default function Task() {
+    const HEADER_CONTENT = (
+        <div className="flex justify-between items-center">
+            <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                Tasks
+            </h2>
+
+            <ModalToggle>
+                Add Task
+            </ModalToggle>
+        </div>
+    )
+
     return (
         <AuthenticatedLayout
-            header={
-                <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                    Tasks
-                </h2>
-            }
+            header={HEADER_CONTENT}
         >
             <Head title="Tasks" />
-            
+
             <MainContainer>
                 <PrimaryCard>
-                    
+
                 </PrimaryCard>
             </MainContainer>
         </AuthenticatedLayout>
