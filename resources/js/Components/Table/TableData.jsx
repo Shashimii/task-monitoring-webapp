d2d513831f14ddfbfe56547d7a3c43e456162690
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-19 16:48:20 +0800

    # text styling

--- resources/js/Components/Table/TableData.jsx
+++ resources/js/Components/Table/TableData.jsx
@@ -1,6 +1,6 @@
 export default function TableData({ children }) {
     return (
-        <td className="px-4 py-3 text-sm dark:text-white">
+        <td className="px-4 py-3 text-sm dark:text-white text-lg font-semibold">
             {children}
         </td>
     )
--- resources/js/Components/Table/TableRow.jsx
+++ resources/js/Components/Table/TableRow.jsx
@@ -1,6 +1,6 @@
 export default function TableRow({children}) {
     return (
-        <tr className="hover:bg-gray-50 dark:hover:bg-gray-800">
+        <tr className="hover:bg-green-100 dark:hover:bg-green-800">
             {children}
         </tr>
     )
