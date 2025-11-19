f5ee28a0c46f5ee7c2773ff324243732420dde09
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-19 16:47:38 +0800

    # Darkmode Color

--- resources/js/Components/Button/ModalToggle.jsx
+++ resources/js/Components/Button/ModalToggle.jsx
@@ -8,8 +8,8 @@ export default function ModalToggle({ children, disabled, ...props }) {
                 bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white 
                 transition duration-150 ease-in-out 
                 hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
-                active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white 
-                dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300 ${disabled && 'opacity-25'}`}
+                active:bg-gray-900 dark:bg-violet-800 dark:text-white dark:hover:bg-violet-600 
+                dark:focus:bg-violet-600 dark:focus:ring-offset-violet-800 dark:active:bg-violet-300 ${disabled && 'opacity-25'}`}
         >
             {children}
         </button>
