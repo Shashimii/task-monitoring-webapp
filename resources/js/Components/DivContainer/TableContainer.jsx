74fb1e045b724e5398a19951ba2069cf5e25b67f
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-19 16:47:51 +0800

    # added title and border

--- resources/js/Components/DivContainer/TableContainer.jsx
+++ resources/js/Components/DivContainer/TableContainer.jsx
@@ -1,6 +1,9 @@
-export default function TableContainer({ children }) {
+export default function TableContainer({ children, tableTitle }) {
     return (
-        <div className="p-6 overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+        <div className="p-6 overflow-hidden bg-white shadow-lg sm:rounded-lg dark:bg-gray-800 border-l-8 border-green-300 dark:border-green-600">
+            <p className="ml-3 mb-4 text-3xl font-semibold">
+                {tableTitle}
+            </p>
             {children}
         </div>
     )
