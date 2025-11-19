f00d1d5bcb84a4bb0dee22371ed79055e3e7df12
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-19 16:47:30 +0800

    # Modal Darkmode Shadcn

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -3,11 +3,11 @@ import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/re
 export default function Modal({
     open,
     setOpen,
-    Icon='a',
-    IconColor='bg-green-500/10 text-green-400',
-    Title='HeaderText',
-    Content='Content',
-    Footer='Footer'
+    Icon,
+    IconColor,
+    Title = 'HeaderText',
+    Content = 'Content',
+    Footer = 'Footer'
 }) {
 
     return (
@@ -22,26 +22,29 @@ export default function Modal({
                     <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                         <DialogPanel
                             transition
-                            className="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline outline-1 -outline-offset-1 outline-white/10 transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+                            className="w-full relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline outline-1 -outline-offset-1 outline-white/10 transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
                         >
-                            <div className="bg-gray-800 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                <div className="sm:flex sm:items-start">
-                                    {Icon && (
-                                        <div className={`mx-auto flex size-12 shrink-0 items-center justify-center rounded-full ${IconColor} sm:mx-0 sm:size-10`}>
-                                            {Icon}
-                                        </div>
-                                    )}
-                                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left w-full">
-                                        <DialogTitle as="h3" className="text-base font-semibold text-white">
+                            <div className="bg-gray-200 px-4 pb-4 pt-5 sm:p-6 sm:pb-4 dark:bg-gray-800">
+                                <div className="flex-col">
+                                    <div className="flex items-center gap-4">
+                                        {Icon && (
+                                            <div className={`flex size-12 shrink-0 items-center justify-center rounded-full ${IconColor} sm:mx-0 sm:size-10`}>
+                                                {Icon}
+                                            </div>
+                                        )}
+
+                                        <DialogTitle as="h3" className="text-base font-semibold text-gray-800 dark:text-white">
                                             {Title}
                                         </DialogTitle>
-                                        <div className="mt-2">
+                                    </div>
+                                    <div className="w-full">
+                                        <div className="my-4">
                                             {Content}
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                            <div className="bg-gray-100 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 dark:bg-gray-700/25">
                                 {Footer}
                             </div>
                         </DialogPanel>
