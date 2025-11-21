e32b621dca0c0854dc10408429c87521bfb6c2e7
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-21 17:44:12 +0800

    # uncontrolled to accept value inline editing

--- resources/js/Components/Form/SelectInput.jsx
+++ resources/js/Components/Form/SelectInput.jsx
@@ -7,13 +7,23 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 
-export default function SelectInput({ label, placeholder, children, value, onChange, error }) {
+export default function SelectInput({ label, placeholder, children, value, defaultValue, onChange, error }) {
+    const selectProps = {}
+
+    if (value !== undefined && value !== null) {
+        selectProps.value = value
+    } else if (defaultValue !== undefined && defaultValue !== null) {
+        selectProps.defaultValue = defaultValue
+    }
+
     return (
         <div className="space-y-2">
+
             <Label
                 title={label}
             />
-            <Select value={value} onValueChange={onChange}>
+
+            <Select {...selectProps} onValueChange={onChange}>
                 <SelectTrigger className="w-full">
                     <SelectValue placeholder={placeholder} />
                 </SelectTrigger>
@@ -21,7 +31,7 @@ export default function SelectInput({ label, placeholder, children, value, onCha
                     {children}
                 </SelectContent>
             </Select>
-            
+
             <p className="text-red-600 dark:text-red-500">{error}</p>
         </div>
     )
