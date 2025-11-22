01422018ee26c88ffbe0d7f1b9a8a76c3a386127
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-22 15:48:20 +0800

    # Color codes

--- resources/js/Components/DivContainer/TableContainer.jsx
+++ resources/js/Components/DivContainer/TableContainer.jsx
@@ -1,6 +1,6 @@
-export default function TableContainer({ children, tableTitle }) {
+export default function TableContainer({ children, tableTitle, borderColor }) {
     return (
-        <div className="p-6 overflow-hidden bg-white shadow-lg sm:rounded-lg dark:bg-gray-800 border-l-8 border-green-300 dark:border-green-600">
+        <div className={`p-6 overflow-hidden bg-white shadow-lg sm:rounded-lg dark:bg-gray-800 border-l-8 ${borderColor}`}>
             <p className="ml-3 mb-4 text-3xl font-semibold">
                 {tableTitle}
             </p>
