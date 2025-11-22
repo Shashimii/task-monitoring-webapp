2d33f7a0e46f2a2559353a2b60a1f2990ed8f05d
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-22 15:48:50 +0800

    # Added colspan for showing no data on the table

--- resources/js/Components/Table/TableRow.jsx
+++ resources/js/Components/Table/TableRow.jsx
@@ -1,7 +1,13 @@
-export default function TableRow({children}) {
+export default function TableRow({ children, colspan }) {
     return (
         <tr className="hover:bg-green-100 dark:hover:bg-green-800">
-            {children}
+            {colspan ? (
+                <td colSpan={colspan} className="text-center py-4">
+                    {children}
+                </td>
+            ) : (
+                children
+            )}
         </tr>
-    )
+    );
 }
