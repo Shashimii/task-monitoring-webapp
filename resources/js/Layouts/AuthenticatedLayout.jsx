afda9471fee7972766565e5a90abc8897d41e9df
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-24 00:18:09 +0800

    # added icons

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -23,7 +23,7 @@ export default function AuthenticatedLayout({ header, children }) {
                             <div className="flex">
                                 <div className="flex shrink-0 items-center">
                                     <Link href="/">
-                                        <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                                        <img src="assets/logo.png" alt="Logo" className="w-10 h-10" />
                                     </Link>
                                 </div>
 
