db3cbaf34a2d534e99836b577927ba9ae455e9c0
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-11-24 01:29:10 +0800

    # text color

--- resources/js/Components/Misc/DivisionContainer.jsx
+++ resources/js/Components/Misc/DivisionContainer.jsx
@@ -1,7 +1,7 @@
 export default function DivisionContainer({ children, bgcolor }) {
     return (
         <span 
-            className="w-full py-2 px-4 text-black font-semibold rounded"
+            className="w-full py-2 px-4 text-gray-800 font-semibold rounded"
             style={{ backgroundColor: bgcolor || '#gray' }}
         >
             {children}
