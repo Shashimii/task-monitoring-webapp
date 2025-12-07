3c9df02c3439088e5648b191c34366c7b40ca97c
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-08 01:08:44 +0800

    # Table Container

--- resources/js/Components/DivContainer/TableContainer.jsx
+++ resources/js/Components/DivContainer/TableContainer.jsx
@@ -1,10 +1,15 @@
-export default function TableContainer({ children, tableTitle, borderColor, headerContent }) {
+export default function TableContainer({ children, tableIcon, tableTitle, borderColor, headerContent }) {
     return (
         <div className={`p-6 overflow-hidden bg-white shadow-lg sm:rounded-lg dark:bg-gray-800 border-l-8 ${borderColor}`}>
             <div className="w-full flex flex-col items-center justify-between sm:flex-row">
-                <p className="ml-3 mb-4 text-3xl font-semibold">
-                    {tableTitle}
-                </p>
+                <span className="flex items-center gap-1 ">
+                    <p className="ml-3 mb-4 text-3xl font-semibold">
+                        {tableIcon}
+                    </p>
+                    <p className="ml-3 mb-4 text-3xl font-semibold">
+                        {tableTitle}
+                    </p>
+                </span>
                 {headerContent}
             </div>
             {children}
