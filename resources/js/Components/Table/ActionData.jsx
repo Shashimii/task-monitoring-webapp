1d3d13bfd7cbe33de8d81db0adbf1c71eb6b2a2b
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-14 21:35:42 +0800

    # action data fixes

--- resources/js/Components/Table/ActionData.jsx
+++ resources/js/Components/Table/ActionData.jsx
@@ -1,5 +1,5 @@
 export default function ActionData({ children, colSpan, className = "" }) {
-    const baseClassName = "w-full px-4 py-3 flex justify-center";
+    const baseClassName = "w-[100px] pl-8 py-3";
     const combinedClassName = className ? `${baseClassName} ${className}` : baseClassName;
     
     return (
