af4d9981c94c9b9a001cc51ad31db3aade460db7
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-14 21:23:41 +0800

    * hover color

--- resources/js/Components/Table/TableRow.jsx
+++ resources/js/Components/Table/TableRow.jsx
@@ -1,5 +1,5 @@
 export default function TableRow({ children, colspan, onClick, className = "" }) {
-    const baseClassName = "divide-x divide-gray-200 dark:divide-gray-700 hover:bg-green-100 dark:hover:bg-green-800";
+    const baseClassName = "divide-x divide-gray-200 dark:divide-gray-700 hover:bg-gray-200 dark:hover:bg-gray-900";
     const combinedClassName = className ? `${baseClassName} ${className}` : baseClassName;
     
     return (
