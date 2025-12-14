9523fa421bb3044df5b1b3fa5d2daaeb5c43de23
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-14 17:32:06 +0800

    # status container refactored

--- resources/js/Components/Misc/StatusContainer.jsx
+++ resources/js/Components/Misc/StatusContainer.jsx
@@ -1,7 +1,24 @@
-export default function StatusContainer({ children, bgcolor }) {
+export default function StatusContainer({ children, status }) {
+
+    const statusColor = (status) => {
+        if (status === "Not Started") {
+            return "bg-gray-400";
+        }
+
+        if (status === "In Progress") {
+            return "bg-orange-400"; 
+        }
+
+        if (status === "Completed") {
+            return "bg-green-400"
+        }
+
+        return "bg-blue-300";
+    }
+
     return (
         <div className="flex items-center gap-2">
-            <span className={`p-3 rounded-full ${bgcolor}`}>
+            <span className={`p-3 rounded-full ${statusColor(status)}`}>
             </span>
 
             <p className="text-black font-semibold dark:text-white">
